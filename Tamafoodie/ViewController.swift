75e720acbec48354daec31127060fcf2b8c075f2
Joey Toh <joeytoh29@gmail.com>
2020-06-17 19:58:21 +0800

    Initial Commit

---
+++ Tamafoodie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tamafoodie
+//
+//  Created by Toh Joey on 17/6/20.
+//  Copyright © 2020 Toh Joey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
